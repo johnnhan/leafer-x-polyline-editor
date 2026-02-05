ffe7432c199f3e1ba5699f617f30db7c28d034cb
Johnhan <mrht@qq.com>
2026-02-05 10:12:21 +0800

    del: 删除老版本代码

--- src/FreePolyline/data.ts
+++
@@ -1,6 +0,0 @@
-import { ArrowData } from '@leafer-in/arrow';
-import type { IFreePolylineData } from './type';
-
-export class FreePolylineData extends ArrowData implements IFreePolylineData {
-  
-}
--- src/OrthoPolyline/data.ts
+++
@@ -1,6 +0,0 @@
-import { ArrowData } from '@leafer-in/arrow';
-import type { IOrthoPolylineData } from './type';
-
-export class OrthoPolylineData extends ArrowData implements IOrthoPolylineData {
-  
-}
